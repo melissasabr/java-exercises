f83171ee8d70bf20f5d8e30acd608fc3937f75e6
melissasabr <timetostudy.mel@gmail.com>
2026-08-14 23:23:41 -0300

    Primeiro commit do projeto HoraDoSistema

---
+++ src/main/java/primeiroprograma/horadosistema/HoraDoSistema.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primeiroprograma.horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author user
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date data = new Date ();
+   System.out.println(data);
+}
+}
